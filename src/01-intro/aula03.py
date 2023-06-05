384cff457a7eed5ed743fb8a7680704f2d1463e6
Jose Carlos <jose.viveiros@aluno.ifsp.edu.br>
2023-06-05 13:11:39 -0300

    aula03 - comentários

---
+++ src/01-intro/aula03.py
@@ -0,0 +1,24 @@
+""" Aula 03 - Comentários """
+
+# Comentário de única linha / Single line comments
+
+'''
+Isso é um comentário
+de mústiplas
+linhas / 
+Multi line comments?
+'''
+
+# Ctrl + ;
+
+# Codigo bem escrito não precisa de comentário
+
+# atribui o valor 10 a variável n1
+n1 = 10
+
+
+n2 = 30
+n3 = 78
+
+print(n1, n2)
+print(n3)
