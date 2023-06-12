a5c77c78ed318611e702d5eb5ce918977a128908
Jose Carlos <jose.viveiros@aluno.ifsp.edu.br>
2023-06-12 12:18:34 -0300

    Aula 04 - while

---
+++ src/02-controle-de-fluxo/aula04.py
@@ -0,0 +1,19 @@
+""" Aula 04 - while"""
+
+
+# while condeicao:
+#    instrucao
+#    instrucao
+#    instrucao
+
+contador = 0
+while contador <= 10:
+    print(contador)
+    contador += 1
+
+print('fim')
+
+# linha = arquivo.readLine()
+# while linha:
+#    instrucao linha
+#    linha = arquivo.readLine()
