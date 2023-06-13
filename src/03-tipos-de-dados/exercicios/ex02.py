9fee9b4f64dd617d608b4538f281a74e6580aa35
Jose Carlos <jose.viveiros@aluno.ifsp.edu.br>
2023-06-13 13:15:35 -0300

    exer 02

---
+++ src/03-tipos-de-dados/exercicios/ex02.py
@@ -0,0 +1,10 @@
+""" Ex 02 """
+
+num_mes = int(input('Entre com o número do mes: '))
+
+meses = ('Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
+         'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro')
+
+valor = num_mes - 1
+
+print(meses[valor])
