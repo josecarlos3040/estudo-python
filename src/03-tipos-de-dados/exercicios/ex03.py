89eb0089b8ad8182e04cb11cb87cc751f1685edd
Jose Carlos <jose.viveiros@aluno.ifsp.edu.br>
2023-06-13 13:37:41 -0300

    exer 03

---
+++ src/03-tipos-de-dados/exercicios/ex03.py
@@ -0,0 +1,21 @@
+""" Ex 03 """
+
+mes = {
+    1: 'Janeiro',
+    2: 'Fevereiro',
+    3: 'Março',
+    4: 'Abril',
+    5: 'Maio',
+    6: 'Junho',
+    7: 'Julho',
+    8: 'Agosto',
+    9: 'Setembro',
+    10: 'Outubro',
+    11: 'Novembro',
+    12: 'Dezembro'
+}
+
+num_mes = int(input('Entre com o número do mes: '))
+
+nome = mes[num_mes]
+print(nome)
